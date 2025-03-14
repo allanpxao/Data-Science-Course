e4b55c32c22e50c43ab19683633295276b67e72d
Alan Santos <alanp.santos@outlook.com>
2025-03-13 22:20:25 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,67 @@
+let drawnNumberList = [];
+let limitNumber = 10;
+let secretNumber = generateRandomNumber();
+let attempts = 1;
+
+function showTextInScreen(tag, text) {
+    let area = document.querySelector(tag);
+    area.innerHTML = text;
+    responsiveVoice.speak(text, 'Brazilian Portuguese Female', { rate: 1.2 });
+}
+
+function displayInitialMessage() {
+    showTextInScreen('h1', 'Secret Number Game');
+    showTextInScreen('p', 'Choose a number between 1 and 10');
+}
+
+displayInitialMessage();
+
+function checkGuess() {
+    let guess = document.querySelector('input').value;
+    
+    if (guess == secretNumber) {
+        showTextInScreen('h1', 'You got it!');
+        let attemptWord = attempts > 1 ? 'attempts' : 'attempt';
+        let attemptsMessage = `You found the secret number with ${attempts} ${attemptWord}!`;
+        showTextInScreen('p', attemptsMessage);
+        document.getElementById('restart').removeAttribute('disabled');
+    } else {
+        if (guess > secretNumber) {
+            showTextInScreen('p', 'The secret number is less than');
+        } else {
+            showTextInScreen('p', 'The secret number is greater than');
+        }
+        attempts++;
+        clearField();
+    }
+}
+
+function generateRandomNumber() {
+    let chosenNumber = parseInt(Math.random() * limitNumber + 1);
+    let numberOfElementsInList = drawnNumberList.length;
+
+    if (numberOfElementsInList == limitNumber) {
+        drawnNumberList = [];
+    }
+    if (drawnNumberList.includes(chosenNumber)) {
+        return generateRandomNumber();
+    } else {
+        drawnNumberList.push(chosenNumber);
+        console.log(drawnNumberList);
+        return chosenNumber;
+    }
+}
+
+function clearField() {
+    let inputField = document.querySelector('input');
+    inputField.value = ''; // Clears the input field
+}
+
+function restartGame() {
+    secretNumber = generateRandomNumber();  // Generates a new secret number
+    attempts = 1;  // Resets the attempts counter
+    clearField();  // Clears the input field
+    displayInitialMessage();  // Shows the initial message again
+    document.getElementById('restart').setAttribute('disabled', true);  // Disables the restart button again
+}
+
